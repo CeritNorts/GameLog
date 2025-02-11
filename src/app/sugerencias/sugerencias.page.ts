6a809fe5707cee3c168eb11734efe66d19e5df86
CeritNorts <20223l001058@utcv.edu.mx>
2025-02-11 01:00:11 -0600

    Sugerencias de logros

--- src/app/sugerencias/sugerencias.page.ts
+++ src/app/sugerencias/sugerencias.page.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class SugerenciasPage implements OnInit {
+  logro = {
+    nombre: '',
+    descripcion: '',
+    juego: '',
+    imagen: ''
+  };
+
+  logrosSugeridos: any[] = [];
+
+  agregarLogro() {
+    if (this.logro.nombre && this.logro.descripcion && this.logro.juego) {
+      this.logrosSugeridos.push({ ...this.logro });
+      this.logro = { nombre: '', descripcion: '', juego: '', imagen: '' };
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
+  }
+
+  cargarImagen(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.logro.imagen = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
   constructor() { }
 
